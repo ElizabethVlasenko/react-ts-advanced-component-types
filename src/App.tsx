0db091cfc6dd40e2e1f25e476b124872a1cb2d87
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-04 10:57:17 +0000

    Add a custom button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import Input from "./components/Input";
 
 function App() {
@@ -5,6 +6,14 @@ function App() {
     <main>
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button el="button" type="submit">
+          Submit
+        </Button>{" "}
+        <Button el="link" href="/">
+          Link
+        </Button>
+      </p>
     </main>
   );
 }
