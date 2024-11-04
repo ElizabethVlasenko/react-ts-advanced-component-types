16481490a8a72839b4f78fac0cdb6cd9a1e6697f
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-04 16:55:25 +0000

    Add a form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
+import Form from "./components/Form";
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Container as={Button}>Click me</Container>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" ref={input} />
-      <p>
-        <Button type="submit">Submit</Button> <Button href="/">Link</Button>
-      </p>
+      <Form>
+        <Input id="name" label="Your name" type="text" />
+        <Input id="age" label="Your age" type="number" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
