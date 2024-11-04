3dc6069029dba0890f7f15760a1fd48fa1d744b9
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-04 10:37:32 +0000

    Add an input specific props to the input component, merging the type with ComponentPropsWithoutRef

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,15 @@
+import { ComponentPropsWithoutRef } from "react";
+
 type InputProps = {
   label: string;
   id: string;
-};
+} & ComponentPropsWithoutRef<"input">; //give a predefined object with all the props of a specific element
 
-export default function Input({ label, id }: InputProps) {
+export default function Input({ label, id, ...props }: InputProps) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} type="text" />
+      <input id={id} {...props} />
     </p>
   );
 }
