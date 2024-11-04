2d86d2e37418caf1f4921030c34013c140993307
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-04 19:58:12 +0000

    Add context to the app; access context value through custom hook

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext, type ReactNode } from "react";
 
 type Timer = {
   name: string;
@@ -17,3 +17,35 @@ type TimersContextValue = TimersState & {
 };
 
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext); //! - makes it to never be null
+
+  if (timersCtx === null) {
+    throw new Error(
+      "useTimersContext must be used within a TimersContextProvider"
+    );
+  }
+
+  return timersCtx;
+}
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: false,
+    addTimer(timerData) {},
+    startTimers() {},
+    stopTimers() {},
+  };
+
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
